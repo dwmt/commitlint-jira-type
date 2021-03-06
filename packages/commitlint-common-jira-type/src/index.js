5fd72242ef7ba5277485d1498b4201ab59eeeaca
Attila Bagossy <>
2021-03-06 13:07:07 +0100

    style: formatting fixes throughout

--- packages/commitlint-common-jira-type/src/index.js
+++ packages/commitlint-common-jira-type/src/index.js
@@ -1,54 +1,54 @@
 const RULES = {
-    projectKeyCase: 'jira-type-project-key-case',
-    projectKeySeparator: 'jira-type-project-key-separator',
-    projectKeyEnum: 'jira-type-project-key-enum',
-    projectKeyEmpty: 'jira-type-project-key-empty',
+  projectKeyCase: 'jira-type-project-key-case',
+  projectKeySeparator: 'jira-type-project-key-separator',
+  projectKeyEnum: 'jira-type-project-key-enum',
+  projectKeyEmpty: 'jira-type-project-key-empty',
 
-    taskNumberEmpty: 'jira-type-task-number-empty',
+  taskNumberEmpty: 'jira-type-task-number-empty',
 
-    taskIdEmpty: 'jira-type-task-id-empty',
+  taskIdEmpty: 'jira-type-task-id-empty',
 
-    typeCase: 'jira-type-type-empty',
-    typeSeparator: 'jira-type-type-separator',
-    typeEmpty: 'jira-type-type-empty',
-    typeEnum: 'jira-type-type-enum',
+  typeCase: 'jira-type-type-empty',
+  typeSeparator: 'jira-type-type-separator',
+  typeEmpty: 'jira-type-type-empty',
+  typeEnum: 'jira-type-type-enum',
 
-    commitMessageSeparator: 'jira-type-commit-message-separator'
+  commitMessageSeparator: 'jira-type-commit-message-separator'
 }
 
 const VALUES = {
-    lowercase: 'lowercase',
-    uppercase: 'uppercase',
-    any: 'any'
+  lowercase: 'lowercase',
+  uppercase: 'uppercase',
+  any: 'any'
 }
 
 const DEFAULTS = {
-    projectKeyCase: VALUES.uppercase,
-    projectKeySeparator: '-',
-    projectKeyEnum: undefined,
+  projectKeyCase: VALUES.uppercase,
+  projectKeySeparator: '-',
+  projectKeyEnum: undefined,
 
-    typeCase: VALUES.lowercase,
-    typeSeparator: '/',
-    typeEnum: undefined,
+  typeCase: VALUES.lowercase,
+  typeSeparator: '/',
+  typeEnum: undefined,
 
-    commitMessageSeparator: ':'
+  commitMessageSeparator: ':'
 }
 
 const LEVEL = {
-    disabled: 0,
-    warning: 1,
-    error: 2
+  disabled: 0,
+  warning: 1,
+  error: 2
 }
 
 const APPLICABLE = {
-    always: 'always',
-    never: 'never'
+  always: 'always',
+  never: 'never'
 }
 
 module.exports = {
-    RULES,
-    VALUES,
-    DEFAULTS,
-    LEVEL,
-    APPLICABLE
+  RULES,
+  VALUES,
+  DEFAULTS,
+  LEVEL,
+  APPLICABLE
 }
