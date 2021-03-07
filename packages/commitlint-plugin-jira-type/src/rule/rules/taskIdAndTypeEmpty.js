11860a43bfb47adeb874283d810cf48265150518
Attila Bagossy <bagossyattila@outlook.com>
2021-03-07 12:39:48 +0100

    style: formatting fixes throughout

--- packages/commitlint-plugin-jira-type/src/rule/rules/taskIdAndTypeEmpty.js
+++ packages/commitlint-plugin-jira-type/src/rule/rules/taskIdAndTypeEmpty.js
@@ -4,7 +4,7 @@ module.exports = {
   name: rules.names.taskIdAndTypeEmpty,
   rule (parsed, _when) {
     if (parsed.hasError(parser.errors.missingTaskIdAndType)) {
-      return Failure(`The commit message contains no task id and type!`)
+      return Failure('The commit message contains no task id and type!')
     }
 
     return Success()
